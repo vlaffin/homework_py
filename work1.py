70d005e0441b1032a4efbc7fe33d8cf92e47489e
vlad <shutikov-vlad@mail.ru>
2023-05-02 18:54:30 +0300

    задачи

---
+++ work1.py
@@ -0,0 +1,5 @@
+a1 = 7
+d = 2
+n = 5
+for i in range(n):
+    print(a1 + i * d, end=' ')
---
+++ work2.py
@@ -0,0 +1,7 @@
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+list_2 = []
+max = 10
+min = 6
+for i in range(len(list_1)):
+    if min <= list_1[i] <= max:
+        print(i, end=' ')
